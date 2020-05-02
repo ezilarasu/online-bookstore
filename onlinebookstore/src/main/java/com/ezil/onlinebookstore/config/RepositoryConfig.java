a9b6a486c08a7acd7ba801eff799a076c36b4780
hp <hp@192.168.1.4>
2020-05-02 16:20:25 +0530

    completed the backend feature one.

--- onlinebookstore/src/main/java/com/ezil/onlinebookstore/config/RepositoryConfig.java
+++ onlinebookstore/src/main/java/com/ezil/onlinebookstore/config/RepositoryConfig.java
@@ -1,5 +1,10 @@
 package com.ezil.onlinebookstore.config;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import javax.persistence.EntityManager;
 import javax.persistence.metamodel.Type;
 
@@ -8,8 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
-import com.ezil.onlinebookstore.entity.Book;
-import com.ezil.onlinebookstore.entity.BookCategory;
+
 
 @Configuration
 public class RepositoryConfig implements RepositoryRestConfigurer{
@@ -22,6 +26,7 @@ public class RepositoryConfig implements RepositoryRestConfigurer{
 		
 	config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream()
 			.map(Type :: getJavaType).toArray(Class[]:: new));
+	
 		
 	}
 	
